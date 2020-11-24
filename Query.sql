0c0143515eada9a69d6bd6f17ccecc0d26356166
Ritishek Yadav <60057151+ritishek@users.noreply.github.com>
2020-11-24 18:29:53 +0530

    Update Query.sql
    second query ! galat ho to sahi kar dena

--- Query.sql
+++ Query.sql
@@ -1,3 +1,8 @@
 
 select c.*,v.* from T13_CUSTOMER c,T13_VEHICLE v, T13_INCIDENT_REPORT ir
 where c.CUST_Id in (select Cust_Id from T13_CLAIM cl where cl.Claim_Status="pending") and c.CUST_Id = v.Cust_Id and c.CUST_Id = ir.Cust_Id;
+
+select c.* from T13_CUSTOMER c where c.Cust_Id in 
+( select p.Cust_Id from T13_PREMIUM_PAYMENT p where p.Premium_Payment_Amount >= 
+( select avg(Premium_Payment_Amount) from T13_PREMIUM_PAYMENT ));
+
