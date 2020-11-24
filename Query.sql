479557fdfe353f729e5f41b3b70ee32975ba4d04
rahulp99 <priyadarshir99@gmail.com>
2020-11-24 15:25:45 +0530

    Query 1 corrected

--- Query.sql
+++ Query.sql
@@ -1,2 +1,3 @@
 
-select c.*,v.* from T13_CUSTOMER c,T13_VEHICLE v where c.CUST_Id in (select Cust_Id from T13_CLAIM cl where cl.Claim_Status="pending");
+select c.*,v.* from T13_CUSTOMER c,T13_VEHICLE v, T13_INCIDENT_REPORT ir
+where c.CUST_Id in (select Cust_Id from T13_CLAIM cl where cl.Claim_Status="pending") and c.CUST_Id = v.Cust_Id and c.CUST_Id = ir.Cust_Id;
